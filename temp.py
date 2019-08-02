34cfe9fc5846629991abaab7431821005de0ff65
Colin Sheng <33919136+sth1988@users.noreply.github.com>
2019-08-02 15:25:10 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,38 +1,76 @@
-import json
-import ssl
+import numpy as np
 import pandas as pd
-from urllib.request import urlopen
+ 
+class PastSampler:
+    '''
+    Forms training samples for predicting future values from past value
+    '''
+     
+    def __init__(self, N, K, sliding_window = True):
+        '''
+        Predict K future sample using N previous samples
+        '''
+        self.K = K
+        self.N = N
+        self.sliding_window = sliding_window
+ 
+    def transform(self, A):
+        M = self.N + self.K     #Number of samples per row (sample + target)
+        #indexes
+        if self.sliding_window:
+            I = np.arange(M) + np.arange(A.shape[0] - M + 1).reshape(-1, 1)
+        else:
+            if A.shape[0]%M == 0:
+                I = np.arange(M)+np.arange(0,A.shape[0],M).reshape(-1,1)
+                
+            else:
+                I = np.arange(M)+np.arange(0,A.shape[0] -M,M).reshape(-1,1)
+            
+        B = A[I].reshape(-1, M * A.shape[1], A.shape[2])
+        ci = self.N * A.shape[1]    #Number of features per sample
+        return B[:, :ci], B[:, ci:] #Sample matrix, Target matrix
 
-start_time = '1439014500'  ## From 2015
+#data file path
+dfp = 'data/bitcoin2015to2019.csv'
 
-coins = ['BTC', 'LTC', 'ETH', 'XMR']
-df_list=[]
-for coin in coins:
-    url = 'https://poloniex.com/public?command=returnChartData&currencyPair=USDT_' \
-          +coin+'&start='\
-          +start_time+\
-          '&end=9999999999&resolution=auto'
-    context = ssl._create_unverified_context()
-    openUrl = urlopen(url, context=context)
-    r = openUrl.read()
-    openUrl.close()
-    d = json.loads(r.decode())
-    print(pd.DataFrame(d).shape)
+#Columns of price data to use
+columns = ['Close']
+# df = pd.read_csv(dfp).dropna().tail(1000000)
+df = pd.read_csv(dfp)
+time_stamps = df['Timestamp']
+df = df.loc[:,columns]
+# original_df = pd.read_csv(dfp).dropna().tail(1000000).loc[:,columns]
+original_df = pd.read_csv(dfp).loc[:,columns]
+
+file_name='data/bitcoin2015to2019_close.h5'
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+# normalization
+for c in columns:
+    df[c] = scaler.fit_transform(df[c].values.reshape(-1,1))
+
     
-url = 'https://poloniex.com/public?command=returnChartData&currencyPair=USDT_ETH' \
-      '&start='+start_time+\
-      '&end=9999999999&resolution=auto'
-context = ssl._create_unverified_context()
-openUrl = urlopen(url, context=context)
-r = openUrl.read()
-openUrl.close()
-d = json.loads(r.decode())
+#%%Features are channels
+A = np.array(df)[:,None,:]
+original_A = np.array(original_df)[:,None,:]
+time_stamps = np.array(time_stamps)[:,None,None]
+#%%Make samples of temporal sequences of pricing data (channel)
+NPS, NFS = 256, 16         #Number of past and future samples
+ps = PastSampler(NPS, NFS, sliding_window=False)
+B, Y = ps.transform(A)
+input_times, output_times = ps.transform(time_stamps)
+original_B, original_Y = ps.transform(original_A)
 
-df = pd.DataFrame(d)
-original_columns=[u'close', u'date', u'high', u'low', u'open']
-new_columns = ['Close','Timestamp','High','Low','Open']
-df = df.loc[:,original_columns]
-df.columns = new_columns
-df.head()
+import h5py
+with h5py.File(file_name, 'w') as f:
+    f.create_dataset("inputs", data = B)
+    f.create_dataset('outputs', data = Y)
+    f.create_dataset("input_times", data = input_times)
+    f.create_dataset('output_times', data = output_times)
+    f.create_dataset("original_datas", data=np.array(original_df))
+    f.create_dataset('original_inputs',data=original_B)
+    f.create_dataset('original_outputs',data=original_Y)
+#     f.create_dataset('original_times', data=time_stamps)
 
-df.to_csv('data/bitcoin2015to2019.csv',index=None)
+B.shape
