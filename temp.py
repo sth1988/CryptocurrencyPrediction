7f9ae3c7164a168a58184addd729dcb2fe32334d
Colin Sheng <33919136+sth1988@users.noreply.github.com>
2019-08-02 15:50:32 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -1,76 +1,151 @@
+from keras import applications
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Dropout, Flatten, Dense, Activation
+from keras.callbacks import CSVLogger
+import tensorflow as tf
+from scipy.ndimage import imread
 import numpy as np
-import pandas as pd
- 
-class PastSampler:
-    '''
-    Forms training samples for predicting future values from past value
-    '''
-     
-    def __init__(self, N, K, sliding_window = True):
-        '''
-        Predict K future sample using N previous samples
-        '''
-        self.K = K
-        self.N = N
-        self.sliding_window = sliding_window
- 
-    def transform(self, A):
-        M = self.N + self.K     #Number of samples per row (sample + target)
-        #indexes
-        if self.sliding_window:
-            I = np.arange(M) + np.arange(A.shape[0] - M + 1).reshape(-1, 1)
-        else:
-            if A.shape[0]%M == 0:
-                I = np.arange(M)+np.arange(0,A.shape[0],M).reshape(-1,1)
-                
-            else:
-                I = np.arange(M)+np.arange(0,A.shape[0] -M,M).reshape(-1,1)
-            
-        B = A[I].reshape(-1, M * A.shape[1], A.shape[2])
-        ci = self.N * A.shape[1]    #Number of features per sample
-        return B[:, :ci], B[:, ci:] #Sample matrix, Target matrix
-
-#data file path
-dfp = 'data/bitcoin2015to2019.csv'
-
-#Columns of price data to use
-columns = ['Close']
-# df = pd.read_csv(dfp).dropna().tail(1000000)
-df = pd.read_csv(dfp)
-time_stamps = df['Timestamp']
-df = df.loc[:,columns]
-# original_df = pd.read_csv(dfp).dropna().tail(1000000).loc[:,columns]
-original_df = pd.read_csv(dfp).loc[:,columns]
-
-file_name='data/bitcoin2015to2019_close.h5'
-
-from sklearn.preprocessing import MinMaxScaler
-scaler = MinMaxScaler()
-# normalization
-for c in columns:
-    df[c] = scaler.fit_transform(df[c].values.reshape(-1,1))
-
-    
-#%%Features are channels
-A = np.array(df)[:,None,:]
-original_A = np.array(original_df)[:,None,:]
-time_stamps = np.array(time_stamps)[:,None,None]
-#%%Make samples of temporal sequences of pricing data (channel)
-NPS, NFS = 256, 16         #Number of past and future samples
-ps = PastSampler(NPS, NFS, sliding_window=False)
-B, Y = ps.transform(A)
-input_times, output_times = ps.transform(time_stamps)
-original_B, original_Y = ps.transform(original_A)
-
+import random
+from keras.layers import LSTM
+from keras.layers import Conv1D, MaxPooling1D, LeakyReLU
+from keras import backend as K
+import keras
+from keras.callbacks import CSVLogger, ModelCheckpoint
+from keras.backend.tensorflow_backend import set_session
+from keras import optimizers
 import h5py
-with h5py.File(file_name, 'w') as f:
-    f.create_dataset("inputs", data = B)
-    f.create_dataset('outputs', data = Y)
-    f.create_dataset("input_times", data = input_times)
-    f.create_dataset('output_times', data = output_times)
-    f.create_dataset("original_datas", data=np.array(original_df))
-    f.create_dataset('original_inputs',data=original_B)
-    f.create_dataset('original_outputs',data=original_Y)
-#     f.create_dataset('original_times', data=time_stamps)
+from sklearn.preprocessing import MinMaxScaler
+import os
+import pandas as pd
+# import matplotlib
 
-B.shape
+import matplotlib.pyplot as plt
+
+os.environ['CUDA_DEVICE_ORDER'] = 'PCI_BUS_ID'
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+set_session(tf.Session(config=config))
+
+with h5py.File(''.join(['bitcoin2015to2017_close.h5']), 'r') as hf:
+    datas = hf['inputs'].value
+    labels = hf['outputs'].value
+    input_times = hf['input_times'].value
+    output_times = hf['output_times'].value
+    original_inputs = hf['original_inputs'].value
+    original_outputs = hf['original_outputs'].value
+    original_datas = hf['original_datas'].value
+    
+scaler=MinMaxScaler()
+#split training validation
+training_size = int(0.8* datas.shape[0])
+training_datas = datas[:training_size,:,:]
+training_labels = labels[:training_size,:,:]
+validation_datas = datas[training_size:,:,:]
+validation_labels = labels[training_size:,:,:]
+validation_original_outputs = original_outputs[training_size:,:,:]
+validation_original_inputs = original_inputs[training_size:,:,:]
+validation_input_times = input_times[training_size:,:,:]
+validation_output_times = output_times[training_size:,:,:]
+
+
+ground_true = np.append(validation_original_inputs,validation_original_outputs, axis=1)
+ground_true.shape
+
+
+ground_true_times = np.append(validation_input_times,validation_output_times, axis=1)
+ground_true_times.shape
+
+step_size = datas.shape[1]
+batch_size= 8
+nb_features = datas.shape[2]
+epochs = 1
+
+#build model
+
+model = Sequential()
+
+# 2 layers
+model.add(Conv1D(activation='relu', input_shape=(step_size, nb_features), strides=3, filters=8, kernel_size=20))
+# model.add(LeakyReLU())
+model.add(Dropout(0.25))
+model.add(Conv1D( strides=4, filters=nb_features, kernel_size=16))
+
+'''
+# 3 layers
+model.add(Conv1D(activation='relu', input_shape=(step_size, nb_features), strides=3, filters=8, kernel_size=8))
+# model.add(LeakyReLU())
+model.add(Dropout(0.5))
+model.add(Conv1D(activation='relu', strides=2, filters=8, kernel_size=8))
+# model.add(LeakyReLU())
+model.add(Dropout(0.5))
+model.add(Conv1D( strides=2, filters=nb_features, kernel_size=8))
+model.summary()
+
+# 4 layers
+model.add(Conv1D(activation='relu', input_shape=(step_size, nb_features), strides=2, filters=8, kernel_size=2))
+# model.add(LeakyReLU())
+model.add(Dropout(0.5))
+model.add(Conv1D(activation='relu', strides=2, filters=8, kernel_size=2))
+# model.add(LeakyReLU())
+model.add(Dropout(0.5))
+model.add(Conv1D(activation='relu', strides=2, filters=8, kernel_size=2))
+# model.add(LeakyReLU())
+model.add(Dropout(0.5))
+model.add(Conv1D( strides=2, filters=nb_features, kernel_size=2))
+'''
+model.load_weights('weights/bitcoin2015to2017_close_CNN_2_relu-44-0.00030.hdf5')
+model.compile(loss='mse', optimizer='adam')
+
+predicted = model.predict(validation_datas)
+predicted_inverted = []
+
+# In[7]:
+for i in range(original_datas.shape[1]):
+    scaler.fit(original_datas[:,i].reshape(-1,1))
+    predicted_inverted.append(scaler.inverse_transform(predicted[:,:,i]))
+print np.array(predicted_inverted).shape
+#get only the close data
+ground_true = ground_true[:,:,0].reshape(-1)
+ground_true_times = ground_true_times.reshape(-1)
+ground_true_times = pd.to_datetime(ground_true_times, unit='s')
+# since we are appending in the first dimension
+predicted_inverted = np.array(predicted_inverted)[0,:,:].reshape(-1)
+print np.array(predicted_inverted).shape
+validation_output_times = pd.to_datetime(validation_output_times.reshape(-1), unit='s')
+
+ground_true_df = pd.DataFrame()
+ground_true_df['times'] = ground_true_times
+ground_true_df['value'] = ground_true
+ground_true_df.head()
+
+ground_true_df = pd.DataFrame()
+ground_true_df['times'] = ground_true_times
+ground_true_df['value'] = ground_true
+ground_true_df.set_index('times').reset_index()
+ground_true_df.shape
+
+prediction_df = pd.DataFrame()
+prediction_df['times'] = validation_output_times
+prediction_df['value'] = predicted_inverted
+prediction_df.shape
+
+prediction_df.head()
+
+
+# 2016/5
+prediction_df = prediction_df.loc[(prediction_df["times"].dt.year == 2017 )&(prediction_df["times"].dt.month > 7 ),: ]
+ground_true_df = ground_true_df.loc[(ground_true_df["times"].dt.year == 2017 )&(ground_true_df["times"].dt.month > 7 ),:]
+
+plt.figure(figsize=(20,10))
+plt.plot(ground_true_df.times,ground_true_df.value, label = 'Actual')
+plt.plot(prediction_df.times,prediction_df.value,'ro', label='Predicted')
+plt.legend(loc='upper left')
+plt.savefig('result/bitcoin2015to2017_close_CNN_2_relu_result.png')
+plt.show()
+
+from sklearn.metrics import mean_squared_error
+mean_squared_error(validation_original_outputs[:,:,0].reshape(-1),predicted_inverted)
